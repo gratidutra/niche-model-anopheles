2b7f9c07b64898306158377b193ce916ab7be5eb
gratidutra <gratirodrigues@gmail.com>
2022-09-10 15:19:26 -0300

    request species link api

--- src/process_data.R
+++ src/process_data.R
@@ -1,6 +1,9 @@
-#https://liibre.github.io/Rocc/articles/articles/using_rspeciesLink.html
+# https://liibre.github.io/Rocc/articles/articles/using_rspeciesLink.html
 library(tidyverse)
 library(rgbif)
+library(leaflet)
+library(Rocc)
+
 
 df <-
   read.table("data/all_anopheles.txt", header = T)
@@ -23,9 +26,8 @@ occ_anopheles <- occ_download(
   pred_in("taxonKey", keys),
   pred("hasCoordinate", TRUE),
   format = "SIMPLE_CSV",
-  user='gratidutra',pwd='iNm!cD!U6@3LhYH',email='gratirodrigues.gdr@gmail.com'
+  user = "gratidutra", pwd = "iNm!cD!U6@3LhYH", email = "gratirodrigues.gdr@gmail.com"
 )
-
 
 z <- occ_download_get(occ_anopheles)
 
@@ -33,8 +35,51 @@ all_species <- occ_download_import(z)
 
 all_species_gbif <-
   all_species %>%
-  drop_na(.) %>%
-  distinct(species, decimalLatitude, decimalLongitude, .keep_all = TRUE)
+  select(species, decimalLatitude, decimalLongitude) %>%
+  drop_na()
 
-# ver package species link 
+
+# ver package species link
 # trocar para variavel de ambiente as credenciais
+
+all_species_gbif <-
+  all_species_gbif[
+    !duplicated(paste(
+      all_species_gbif$species, all_species_gbif$decimalLongitude,
+      all_species_gbif$decimalLatitude
+    )),
+  ]
+
+
+# leaflet(all_species_gbif) %>%
+#   addTiles() %>%
+#   addMarkers(
+#     lng = ~decimalLongitude, lat = ~decimalLatitude,
+#     popup = paste(all_species_gbif$decimalLongitude, all_species_gbif$decimalLatitude)
+#   )
+
+
+# Species Link ------------------------------------------------------------
+
+all_species_splink_raw <- 
+  rspeciesLink(filename = "all_species_splink",
+                     species = splist,
+                     Coordinates = "Yes",
+                     CoordinatesQuality = "Good")
+
+all_species_splink <-
+  all_species_splink_raw[
+    !duplicated(paste(
+      all_species_splink_raw$scientificName,
+      all_species_splink_raw$decimalLongitude,
+      all_species_splink_raw$decimalLatitude
+    )),
+  ]
+
+all_species_splink <- all_species_splink %>%
+  rename(species = scientificName) %>%
+  select(species, decimalLatitude, decimalLongitude) %>%
+  mutate(decimalLatitude = as.numeric(decimalLatitude),
+         decimalLongitude = as.numeric(decimalLongitude))
+
+all_species_gbif %>% bind_rows(all_species_splink)
